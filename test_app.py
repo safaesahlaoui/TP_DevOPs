618006074d76e7b4d7ef8ec1c532d639cc3f1720
Safae <safaesahlaoui2001@gmail.com>
2023-03-15 00:37:39 +0100

    fixing pylint issues for test_app.py

--- test_app.py
+++ test_app.py
@@ -1,5 +1,7 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
 from seleniumconfig import Infow
-
 def test_py():
     info = Infow()
     info.get_info('Maroc')
